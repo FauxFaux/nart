74fc5fc586fb322f2af7e0a3275e88574457176c
Chris West (Faux) <git@goeswhere.com>
2019-04-05 22:20:41 +0100

    some kind of support type

--- examples/dvd.rs
+++ examples/dvd.rs
@@ -10,29 +10,20 @@ fn main() -> Result<(), Error> {
 
     let mut nart = Nart::new("dvd", 640, 480, true)?;
 
-    let mut rng = rand_xoshiro::Xoshiro256Plus::seed_from_u64(0);
+    let mut rng = ByteRand::new();
 
     while nart.win.is_open() && !nart.win.is_key_down(Key::Escape) {
         let mut buffer = nart.buffer();
 
-        let mut pos = 0;
-        'fill: loop {
-            let mut rand = rng.next_u64();
-            for _ in 0..8 {
-                let brightness = rand as u8;
-                rand >>= 8;
-                buffer.as_mut()[pos] = RgbaVec {
-                    r: brightness,
-                    g: brightness,
-                    b: brightness,
-                    a: 255,
-                }
-                .to_packed();
-                pos += 1;
-                if buffer.as_mut().len() == pos {
-                    break 'fill;
-                }
+        for cell in buffer.as_mut() {
+            let mut rand = rng.next();
+            *cell = RgbaVec {
+                r: rand,
+                g: rand,
+                b: rand,
+                a: 255,
             }
+            .to_packed();
         }
 
         buffer.image_one_minus_src(&image, (10, 10));
@@ -41,4 +32,32 @@ fn main() -> Result<(), Error> {
     }
 
     Ok(())
+}
+
+struct ByteRand {
+    rng: rand_xoshiro::Xoshiro256Plus,
+    curr: u64,
+    bip: u8,
+}
+
+impl ByteRand {
+    fn new() -> ByteRand {
+        let mut rng = rand_xoshiro::Xoshiro256Plus::seed_from_u64(0);
+        ByteRand {
+            curr: rng.next_u64(),
+            bip: 0,
+            rng,
+        }
+    }
+
+    fn next(&mut self) -> u8 {
+        if 8 == self.bip {
+            self.bip = 0;
+            self.curr = self.rng.next_u64();
+        }
+        let ret = self.curr as u8;
+        self.curr >>= 8;
+        self.bip += 1;
+        ret
+    }
 }
